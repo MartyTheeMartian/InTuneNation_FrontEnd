3f34ec1ca7f2763ac0fc1487bca7886a055fbdd4
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-04-26 01:01:20 -0700

    updating personal page

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import mathew from '../../assets/matthew.png'
-import {Button, Thumbnail, Col, Row, Grid} from 'react-bootstrap';
+import {Button, Thumbnail, Col, Row, Grid, MenuItem, Clearfix, onSelectAlert, ButtonGroup, DropdownButton} from 'react-bootstrap';
 const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
 
 
@@ -24,12 +24,28 @@ class Profile extends Component {
             <Thumbnail src={mathew} alt="242x200">
               <h3>Kevin Zheng</h3>
               <p>Kevin is born in pleasanton, he has always been passionate about helping people lives. He want to help people improve their singing skills</p>
-              <div className="well" style={wellStyles}>
-      <Button bsStyle="primary" bsSize="large" block>Block level button</Button>
-      <Button bsSize="large" block>Block level button</Button>
-    </div>
             </Thumbnail>
-          </Col>
+
+            <ButtonGroup vertical>
+        <Button>Button</Button>
+        <Button>Button</Button>
+        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-1">
+          <MenuItem eventKey="1">Dropdown link</MenuItem>
+          <MenuItem eventKey="2">Dropdown link</MenuItem>
+        </DropdownButton>
+        <Button>Button</Button>
+        <Button>Button</Button>
+        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-2">
+          <MenuItem eventKey="1">Dropdown link</MenuItem>
+          <MenuItem eventKey="2">Dropdown link</MenuItem>
+        </DropdownButton>
+        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-3">
+          <MenuItem eventKey="1">Dropdown link</MenuItem>
+          <MenuItem eventKey="2">Dropdown link</MenuItem>
+        </DropdownButton>
+      </ButtonGroup>
+        </Col>
+
           <Col xs={14} md={4}>
 
           </Col>
