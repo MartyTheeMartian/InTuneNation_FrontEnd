a5481aeabe07ca67f1d06393dac6e38d18ef45ab
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-04-29 23:59:13 -0700

    finished sign up page

--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 import {Field, reduxForm} from 'redux-form';
 import {signUserUp} from '../../actions';
 import {LinkContainer} from 'react-router-bootstrap';
+import google_logo from '../../assets/img/google_logo.png';
 
 const mapStateToProps = (state, ownProps) => {
   console.log('what is state in store?', state);
@@ -31,48 +32,41 @@ class SignUp extends Component {
   render() {
     const {handleSubmit, pristine, reset, submitting} = this.props;
     return (
-      <div>
-              <button className="btn btn-info col-md-5">
-                <h4>Sign Up with Google</h4>
-              </button>
+      <div className="signup-modal">
+        <button className="btn signup-modal-btn-google">
+          <div className="signup-modal-btn-google-inside">
+            <img src={google_logo}></img>
+            <span>Sign Up with Google</span>
+          </div>
+        </button>
 
-
-          <h6 className="text-center">
-            OR
-          </h6>
-          <form onSubmit={handleSubmit(this.onSubmit)}>
-            <div className="modal-body" id="personalSignup">
-
-                  <h4>First Name</h4>
-                  <Field name="firstName" component="input" type="text" placeholder="First Name" className="col-md-12"/>
-                  <h4>Last Name</h4>
-                  <Field name="lastName" component="input" type="text" placeholder="Last Name" className="col-md-12"/>
-                  <h4>Email</h4>
-                  <Field name="email" component="input" type="email" placeholder="Email" className="col-md-12"/>
-                  <h4>Password</h4>
-                  <Field name="password" component="input" type="password" placeholder="Passwords" className="col-md-12"/>
-                  <h4>Confirm Password</h4>
-                  <Field name="confirmPassword" component="input" type="password" placeholder="Confirm passwords" className="col-md-12"/>
-            </div>
-
-            {/* <button type="submit" className="btn btn-default" >Sign Up</button> */}
-            <div className="modal-footer">
-              <div className="container footer ">
-                <div className="row justify-content-md-center " id="submitButton">
-                  <button type="button" className="btn btn-primary col-md-7" type="submit">
-                    <h5>Submit</h5>
-                  </button>
-                </div>
-                <br/>
-                <div className="row justify-content-md-center">
-                  <button type="button" className="btn btn-default col-md-7 " data-dismiss="modal">
-                    <h5>Close</h5>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </form>
+        <div className="signup-modal-or-decorate">
+          <div className="signup-modal-or-decorate-lineL">you should'n see this long long string</div>
+          <span>or</span>
+          <div className="signup-modal-or-decorate-lineR">you should'n see this long long string</div>
         </div>
+
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <div className="signup-modal-content">
+
+            <Field name="firstName" component="input" type="text" placeholder="First Name" className="signup-modal-content-detail" required/>
+            <Field name="lastName" component="input" type="text" placeholder="Last Name" className="signup-modal-content-detail" required/>
+            <Field name="email" component="input" type="email" placeholder="Email" className="signup-modal-content-detail" required/>
+            <Field name="password" component="input" type="password" placeholder="Passwords" className="signup-modal-content-detail" required/>
+            <Field name="confirmPassword" component="input" type="password" placeholder="Confirm passwords" className="signup-modal-content-detail" required/>
+          </div>
+
+          <div className="signup-modal-foot-btn">
+            <button type="button" className="btn btn-danger" type="submit">
+              <h5>Submit</h5>
+            </button>
+            <button type="button" className="btn btn-default" data-dismiss="modal">
+              <h5>Close</h5>
+            </button>
+          </div>
+
+        </form>
+      </div>
     );
   }
 }
