c06076df71acb1bbf517c0686fbec2ff7b12c211
Martin Yee <MartyTheeMartian@Martins-MacBook-Air.local>
2017-05-02 19:23:25 -0700

    CSS refractor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import '../src/assets/css/main.css';
+import getUserMedia from './utils/userMedia';
 import store from './store';
 
 ReactDOM.render(
