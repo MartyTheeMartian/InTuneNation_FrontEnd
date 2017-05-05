e7911a72ea0e9aec4f97b5b5811c8980721786ba
Martin Yee <MartyTheeMartian@Martins-MacBook-Air.local>
2017-05-05 13:34:14 -0700

    Friday afternoon

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -4,6 +4,9 @@ const loginReducer = (state = initialState.user, action) => {
   switch (action.type) {
     case 'USER_LOG_IN_FULFILLED':
     localStorage.setItem('token', action.payload.data.token);
+    localStorage.setItem('userId', action.payload.data.id);
+    localStorage.setItem('firstName', action.payload.data.first_name);
+    localStorage.setItem('lastName', action.payload.data.last_name);
       return { ...action.payload.data };
     default:
       return state;
