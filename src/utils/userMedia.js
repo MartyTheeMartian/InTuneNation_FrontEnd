ccc34db87ab792b039246f7093f3756d288334da
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-06-01 19:25:32 -0700

    fixed userId string error

--- src/utils/userMedia.js
+++ src/utils/userMedia.js
@@ -92,7 +92,7 @@ export default getUserMedia({ video: false, audio: true })
           dispatch(pushScoreToExerciseScoresArray(scoreToAdd));
           if (getState().exerciseScoresReducer.length === keyEvents.length) {
             // POST SCORES TO DB
-            const userId = localStorage.getItem('userId');
+            const userId = parseInt(localStorage.getItem('userId'));
             const exerciseId = getState().currentExerciseIdReducer.id;
             const finalScoreArray = getState().exerciseScoresReducer;
             scorePostingUtility(userId, exerciseId, finalScoreArray);
