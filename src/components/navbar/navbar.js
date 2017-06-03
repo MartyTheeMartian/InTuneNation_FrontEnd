3a13c555ac0ee4f1e1b42e6919d091e98b29e871
Martin Yee <MartyTheeMartian@Martins-MacBook-Air.local>
2017-06-03 16:30:40 -0700

    Created Sign Out button + flow

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
 import LogIn from '../logIn/logIn.js';
 import SignUp from '../signUp/signUp.js';
+import LogOut from '../logOut/logOut.js';
 // import music from '../../assets/img/music.jpg'
 // import profile from '../../assets/img/profile-icon.png'
 
 
 const mapStateToProps = (state) => {
   return {
-    logIn: state.loginReducer.loginSuccess
   };
 };
 
@@ -20,11 +20,39 @@ const mapDispatchToProps = (dispatch) => { return bindActionCreators({}, dispatc
 
 class NavBar extends Component {
 
-  hideButtons = () => {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect : false
+    }
   }
 
+  displayTabs = () => {
+    if (localStorage.length === 0) {
+      return { display: 'none' };
+    }
+    else {
+      return { display: 'block' };
+    }
+  }
 
+  displaySignUpLogIn = () => {
+    if (localStorage.length === 0) {
+      return { display: 'block' };
+    }
+    else {
+      return { display: 'none' };
+    }
+  }
+
+  displayLogOut = () => {
+    if (localStorage.length === 0) {
+      return { display: 'none' };
+    }
+    else {
+      return { display: 'block' };
+    }
+  }
 
   render() {
     return (
@@ -36,35 +64,45 @@ class NavBar extends Component {
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav pullRight>
+
+          <Nav pullLeft>
             <LinkContainer exact to="/">
               <NavItem href="#">Home</NavItem>
             </LinkContainer>
 
-            <LinkContainer to="/interface">
+            <LinkContainer to="/interface" style={this.displayTabs()}>
               <NavItem href="#">Interface</NavItem>
             </LinkContainer>
 
-            <LinkContainer to="/profile">
+            <LinkContainer to="/profile" style={this.displayTabs()}>
               <NavItem href="#">Profile</NavItem>
             </LinkContainer>
 
             <Nav className="githubLink">
               <a className="githubLink" href="https://github.com/MartyTheeMartian/Intervalicity-FrontEnd/blob/master/README.md" target="_blank">GitHub</a>
             </Nav>
+          </Nav>
+
+          <Nav pullRight>
               {/* <div className="fa fa-user-circle headIcon"></div> */}
-            <NavItem id="signButton" href="" data-toggle="modal" data-target="#myModal1">
+            <NavItem id="signButton" href="" data-toggle="modal" data-target="#signUp" style={this.displaySignUpLogIn()}>
               <span className="navbar-right">Sign Up</span>
             </NavItem>
-            <NavItem id="logButton" href="" data-toggle="modal" data-target="#myModal2" >
-              <span className="navbar-right" >Log In</span>
-            </NavItem>
               <SignUp />
+            <NavItem id="logButton" href="" data-toggle="modal" data-target="#logIn" style={this.displaySignUpLogIn()}>
+              <span className="navbar-right">Log In</span>
+            </NavItem>
               <LogIn />
+            <NavItem id="signButton" href="" data-toggle="modal" data-target="#logOut" style={this.displayLogOut()}>
+              <span className="navbar-right">Log Out</span>
+            </NavItem>
+              <LogOut />
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     );
+
   }
 
 }
