eaac3952edec3ee8cf5a3581c3d76dff436235ef
Marty <martytheemartian@yahoo.com>
2017-06-15 09:52:57 -0700

    Cleared Console Logs

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,7 +26,6 @@ export const loadSpecificExercisesIDwithAllScoresData = (user_id, exercise_id) =
 export const postSignUp = (user) => {
   const API_URL = `https://ppp-capstone-music.herokuapp.com/user/signup`;
   let data = axios.post(API_URL, user, config());
-  console.log('am i in postsignup action');
   return {
     type: 'USER_SIGN_UP',
     payload: data
@@ -34,11 +33,8 @@ export const postSignUp = (user) => {
 };
 
 export const postLogIn = (user) => {
-  console.log('what is user here now,', user);
   let API_URL = `https://ppp-capstone-music.herokuapp.com/user/login`;
   let data = axios.post(API_URL, user, config());
-  console.log('am i in postsignup action');
-
   return {
     type: 'USER_LOG_IN',
     payload: data
@@ -169,7 +165,6 @@ export const postExercise = (userId, body) => {
 }
 
 export const setExerciseId = (userId, body) => {
-  console.log('inside setExerciseId');
   return {
     type: 'SET_EXERCISE_ID',
     payload: postExercise(userId, body),
