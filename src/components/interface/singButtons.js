a55b34af0b32aaa28bbf3c10ff97bb1057ce5156
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-06-16 14:18:30 -0700

    added starting note button, general edits

--- src/components/interface/singButtons.js
+++ src/components/interface/singButtons.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { toggleAudioCapture, singButton, resetState } from '../../actions';
+import Tone from '../../../tone-js/tone';
+const note = require('../../assets/img/image.png');
 
 const mapStateToProps = (state) => {
   return {
@@ -10,20 +12,18 @@ const mapStateToProps = (state) => {
     singDisabled: state.singButtonReducer.disabled,
     captureDisabled: state.captureReducer.disabled,
     captureText: state.captureReducer.captureText,
-    // keyStrokeEvents: state.keyStrokeEvents,
-    // vocalInputResults: state.vocalInputResults,
-    // exerciseScores: state.exerciseScores,
-    // greenTime: state.greenTime,
-    // targetNote: state.targetNote,
-    // targetNoteIndex: state.targetNoteIndex,
-    // sungNote: state.sungNote,
-    // recordingStatus: state.recordingStatusReducer,
+    keyEvents: state.keyEventsReducer,
+    recordingStatus: state.recordingStatusReducer,
   };
 };
 
 const mapDispatchToProps = (dispatch) => { return bindActionCreators({ toggleAudioCapture, singButton, resetState }, dispatch); };
 
 class SingButtons extends Component {
+  constructor(props) {
+    super(props);
+    this.synth = new Tone.Synth().toMaster();
+  }
 
   handleResetClick = () => { this.props.resetState(); }
 
@@ -31,6 +31,32 @@ class SingButtons extends Component {
     this.props.toggleAudioCapture();
     this.props.singButton();
   }
+
+  handleStartingNoteClick = () => {
+    if (!this.props.recordingStatus) {
+      const tone = this.props.keyEvents[0].tNote.toUpperCase();
+      this.synth.triggerAttackRelease(tone, 0.65);
+    }
+  }
+
+  // handlePlayExerciseClick = () => {
+  //   if (!this.props.recordingStatus) {
+  //     const firstNote = this.props.keyEvents[0].tNote.toUpperCase();
+  //     let chain = this.synth.triggerAttackRelease(firstNote, 0.65, 0);
+  //     for (var i = 1; i < this.props.keyEvents.length; i++) {
+  //       let newNote = this.props.keyEvents[i].tNote.toUpperCase();
+  //       chain = chain.setNote(newNote, "+4n");
+  //     }
+  //     chain;
+      // this.props.keyEvents.forEach((keyEvent, index) => {
+      //   const tone = keyEvent.tNote.toUpperCase();
+      //   console.log(index);
+      //   this.synth.triggerAttackRelease(tone, 0.5);
+      //   // notes.push(instanceFn);
+      // });
+      // notes.forEach((fn) => { fn(); });
+    // }
+  // }
 
   getDisabled = () => {
     if (this.props.captureDisabled === '') { return 'disabled'; }
@@ -40,6 +66,12 @@ class SingButtons extends Component {
   render() {
     return (
       <div className="row">
+        {/* <div className="col-md-12">
+          <button id="play-exercise-button" onClick={this.handlePlayExerciseClick} className="btn btn-lg active captureBox" disabled={this.getDisabled()}>PLAY EXERCISE</button>
+        </div> */}
+        <div className="col-md-12">
+          <button id="starting-note-button" onClick={this.handleStartingNoteClick} className="btn btn-lg active captureBox" disabled={this.getDisabled()}>START 🎵</button>
+        </div>
           <div className="col-md-12">
             <button id="sing-button" onClick={this.handleSingClick} className="btn btn-lg active captureBox" disabled={this.getDisabled()}>{this.props.singText}</button>
           </div>
