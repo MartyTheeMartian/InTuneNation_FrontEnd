2e4710cb04f47b69a4c173158010bd6050fed655
Martin Yee <MartyTheeMartian@Martins-MacBook-Air.local>
2017-06-17 19:52:19 -0700

    Improved Interface CSS

--- src/components/interface/pianoButtons.js
+++ src/components/interface/pianoButtons.js
@@ -36,14 +36,14 @@ class PianoButtons extends Component {
   render() {
     return (
       <div id="octRow" className="row">
-        <div className="col-md-4 col-sm-4 col-xs-4">
+        <div id="leftOctBut" className="col-md-4 col-sm-4 col-xs-4">
           <button onClick={() => this.octaveShift('-')} className="btn octaveButtons btn-lg active" disabled={this.props.down}> - Octave <br /> {this.props.leftOctave}</button>
         </div>
-        <div className="col-md-4 col-sm-4 col-xs-4">
+        <div id="captureBut" className="col-md-4 col-sm-4 col-xs-4">
           <button onClick={this.handleClick} className="btn captureButton btn-lg active" disabled={this.props.disabled}>{this.props.captureText}<br />Capture</button>
         </div>
-        <div className="col-md-4 col-sm-4 col-xs-4">
-          <button id="rOctBut" onClick={() => this.octaveShift('+')} className="btn octaveButtons btn-lg active" disabled={this.props.up}> Octave +  <br /> {this.props.rightOctave}</button>
+        <div id="rightOctBut" className="col-md-4 col-sm-4 col-xs-4">
+          <button onClick={() => this.octaveShift('+')} className="btn octaveButtons btn-lg active" disabled={this.props.up}> Octave +  <br /> {this.props.rightOctave}</button>
         </div>
       </div>
     );
