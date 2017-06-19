aced8a5a6a7602513d2ab72e602c02f49544f08f
Marty <martytheemartian@yahoo.com>
2017-06-19 09:39:44 -0700

    Update dashboard.js

--- src/reducers/dashboard.js
+++ src/reducers/dashboard.js
@@ -3,7 +3,6 @@ import initialState from './initialState';
 const dashboardReducer = (state = initialState.dashboard, action) => {
   switch (action.type) {
     case 'PAST_EXERCISES_TABLE_RUN_FULFILLED':
-    console.log('past exercise', action)
       return action.payload.data;
     default:
       return state;
