ddc9ca96a7eab21d485d444e5badf7ae1c81370d
Marty <martytheemartian@yahoo.com>
2017-06-19 16:21:53 -0700

    Update dashboard.js

--- src/reducers/dashboard.js
+++ src/reducers/dashboard.js
@@ -4,7 +4,6 @@ const dashboardReducer = (state = initialState.dashboard, action) => {
   switch (action.type) {
     case 'PAST_EXERCISES_TABLE_RUN_FULFILLED':
       return [ ...state, ...action.payload.data ];
-      // return state.concat(action.payload)
     default:
       return state;
   }
