f329571ddeafd955de72e138830fd7b87028ce26
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-06-18 21:01:44 -0700

    update google test

--- src/reducers/googleOauth.js
+++ src/reducers/googleOauth.js
@@ -3,7 +3,7 @@ import initialState from './initialState';
 const googleOauthReducer = (state = { }, action) => {
   switch (action.type) {
     case 'LOAD_PROFILE':
-      return { ...action.payload };
+      return action.payload ;
     default:
       return state;
   }
--- tests/redux/reducers/googleOauth.test.js
+++ tests/redux/reducers/googleOauth.test.js
@@ -14,7 +14,18 @@ describe('googleOauthReducer', function() {
 
   it('returns a new state with the specified articles set on it', function() {
     const prevState = {};
-    const action = {
+    // const action = {
+    //   type: 'LOAD_PROFILE',
+    //   payload: {
+    //     token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImlhdCI6MTQ5NzgxNDg5MywiZXhwIjoxNDk4NDE5NjkzfQ.vAG1sQ1lUHh9hZmLmFk4ToIUb9bYRBcT2x3q7tnJFE0",
+    //     id: "2",
+    //     firstName: "Kevin",
+    //     lastName: "Zheng",
+    //     email: "kvinzheng@gmail.com",
+    //     profile_picture: "https://lh4.googleusercontent.com/-Ro0sSQVz7s0/AAAAAAAAAAI/AAAAAAAAAZ4/Ip6fTqSbzAk/photo.jpg?sz=2200"
+    //   }
+    // }
+    const nextState = googleOauthReducer(prevState,  {
       type: 'LOAD_PROFILE',
       payload: {
         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImlhdCI6MTQ5NzgxNDg5MywiZXhwIjoxNDk4NDE5NjkzfQ.vAG1sQ1lUHh9hZmLmFk4ToIUb9bYRBcT2x3q7tnJFE0",
@@ -24,16 +35,15 @@ describe('googleOauthReducer', function() {
         email: "kvinzheng@gmail.com",
         profile_picture: "https://lh4.googleusercontent.com/-Ro0sSQVz7s0/AAAAAAAAAAI/AAAAAAAAAZ4/Ip6fTqSbzAk/photo.jpg?sz=2200"
       }
-    }
-    const nextState = googleOauthReducer(prevState, action);
+    });
     expect(nextState).not.toBe(prevState);
-    expect(nextState).toEqual({...{
+    expect(nextState).toEqual( {
       token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImlhdCI6MTQ5NzgxNDg5MywiZXhwIjoxNDk4NDE5NjkzfQ.vAG1sQ1lUHh9hZmLmFk4ToIUb9bYRBcT2x3q7tnJFE0",
       id: "2",
       firstName: "Kevin",
       lastName: "Zheng",
       email: "kvinzheng@gmail.com",
       profile_picture: "https://lh4.googleusercontent.com/-Ro0sSQVz7s0/AAAAAAAAAAI/AAAAAAAAAZ4/Ip6fTqSbzAk/photo.jpg?sz=2200"
-    }});
+    } );
   });
 });
